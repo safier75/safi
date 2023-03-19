611140cac2b2e25862234d16d84f3c437f24617f
satish <safier75@gmail.com>
2023-03-19 15:05:55 +0530

    first commit

---
+++ safier/resources/views/saf/about.blade.php
@@ -0,0 +1,31 @@
+@extends('saf.layout')
+@section('containt')
+<main id="main">
+<!-- ======= About Section ======= -->
+ <section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row">
+          <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
+            <img src="assets/img/about.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content">
+            <h3>SafierTech Solutions have faith in doing business with ethics and added values.</h3>
+            <p class="fst-italic">
+              Repair and mentinence of sound system, by providing the telented skilled technicians to achive quality of music.
+            </p>
+            <ul>
+              <li><i class="bi bi-check-circle"></i> Sterio music of heigh quality assets.</li>
+              <li><i class="bi bi-check-circle"></i> D J and sound system in marriage party.</li>
+              <li><i class="bi bi-check-circle"></i> We provide the music system to vehicles of all kind , our music system is installed in tractors and vehicles such as bus truck car etc.</li>
+            </ul>
+            <p>
+              Our moto is the custumer safisfaction. we  provide the serviceability of long lasting. and have the responsibility to make the electronic goods afresh.        </p>
+
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+    </main><!-- End #main -->
+@endsection
---
+++ safier/resources/views/saf/home.blade.php
@@ -0,0 +1,11 @@
+@extends('saf.layout')
+@section('containt')
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex justify-content-center align-items-center">
+    <div class="container position-relative" data-aos="zoom-in" data-aos-delay="100">
+      <h1>Doing Today,<br>Achiving Tomorrow</h1>
+      <h2>We are team of talented technicians making cutting edge Technology to achive the great Goal</h2>
+      <a href="courses.html" class="btn-get-started">Get Started</a>
+    </div>
+  </section><!-- End Hero -->
+@endsection
